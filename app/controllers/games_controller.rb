2f1aee52681216561d0d90c46893d008dd5e2988
takuya1 <oktkgm1997@icloud.com>
2025-12-27 10:41:19 +0000

    add: play画面、result画面の追加

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,31 @@
 class GamesController < ApplicationController
+  TOTAL_TURNS = 5
+  START_MONEY = 50000
+
   def start
+    # 初期化
+    session[:money] = START_MONEY
+    session[:turn]  = 1
+  end
+
+  def play
+    @money = session[:money]
+    @turn  = session[:turn]
+
+    # ターン数が上限を超えたら結果画面へ
+    redirect_to result_path if @turn > TOTAL_TURNS
+  end
+
+  def choose
+    amount = params[:amount].to_i
+
+    session[:money] -= amount
+    session[:turn]  += 1
+
+    redirect_to play_path
+  end
+
+  def result
+    @money = session[:money]
   end
 end
