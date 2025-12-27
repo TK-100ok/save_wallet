12f8fe48b80a20f78b857a68ad0d2e6f6ec10f5e
takuya1 <oktkgm1997@icloud.com>
2025-12-27 14:40:34 +0000

    add: 簡単なゲーム機能追加

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,27 +2,73 @@ class GamesController < ApplicationController
   TOTAL_TURNS = 5
   START_MONEY = 50000
 
+  CHILDREN = [
+   {
+     name: "はなこ",
+     image: "kids/kid2.png",
+     line: "お年玉って、いい文化だよね？",
+     optimal: 5000
+   },
+   {
+     name: "ゆうと",
+     image: "kids/kid3.png",
+     line: "今年はゲーミングPCが欲しくて…！",
+     optimal: 10000
+   },
+   {
+     name: "みゆ",
+     image: "kids/kid4.png",
+     line: "私、一番いい子にしてたよね？",
+     optimal: 1000
+   },
+   {
+     name: "けんと",
+     image: "kids/kid5.png",
+     line: "お兄ちゃん信じてるよ？",
+     optimal: 5000
+   },
+   {
+     name: "たろう",
+     image: "kids/kid1.png",
+     line: "お正月だよ！ちょうだい！",
+     optimal: 3000
+   }
+  ]
+
   def start
     # 初期化
     session[:money] = START_MONEY
-    session[:turn]  = 1
+    session[:turn]  = 0
   end
 
   def play
     @money = session[:money]
     @turn  = session[:turn]
+    @child = CHILDREN[session[:turn]]
 
     # ターン数が上限を超えたら結果画面へ
-    redirect_to result_path if @turn > TOTAL_TURNS
+    redirect_to result_path if @turn + 1 > TOTAL_TURNS
   end
 
   def choose
-    amount = params[:amount].to_i
+  amount = params[:amount].to_i
 
-    session[:money] -= amount
-    session[:turn]  += 1
+  # 今の子供データを取得
+  child = CHILDREN[session[:turn]]
 
-    redirect_to play_path
+  # 最適解を渡せなかったらゲームオーバー
+  if amount < child[:optimal]
+    redirect_to game_over_path and return
+  end
+
+  # 渡せたら次へ
+  session[:money] -= amount
+  session[:turn]  += 1
+
+  redirect_to play_path
+  end
+
+  def game_over
   end
 
   def result
